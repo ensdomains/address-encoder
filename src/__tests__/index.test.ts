11cbfbe73a4d5afa40184026ba7671582b28fd2a
Nick Johnson <arachnid@notdot.net>
2019-10-21 16:39:59 +1300

    Additional testcase

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -18,6 +18,7 @@ const vectors: Array<TestVector> = [
         {text: 'bc1pw508d6qejxtdg4y5r3zarvary0c5xw7kw508d6qejxtdg4y5r3zarvary0c5xw7k7grplx', hex: '5128751e76e8199196d454941c45d1b3a323f1433bd6751e76e8199196d454941c45d1b3a323f1433bd6'},
         {text: 'bc1sw50qa3jx3s', hex: '6002751e'},
         {text: 'bc1zw508d6qejxtdg4y5r3zarvaryvg6kdaj', hex: '5210751e76e8199196d454941c45d1b3a323'},
+        {text: 'bc1qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3qccfmv3', hex: '00201863143c14c5166804bd19203356da136c985678cd4d27a1b8c6329604903262'},
       ],
     },
     {
