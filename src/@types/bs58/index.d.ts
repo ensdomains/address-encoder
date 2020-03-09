c0eebb403deeaa65fc64243bd6277084efec1a5f
Maksim Tolochko <maksim.tolo@gmail.com>
2020-03-09 09:44:14 +0300

    After merge fix

--- src/@types/bs58/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'bs58' {
-  export function encode(data: Buffer): string;
-  export function decode(data: string): Buffer;
-  export function decodeUnsafe(data: string): Buffer;
-}
--- src/@types/sha.js/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'sha.js/sha256' {
-  import { Hash } from 'crypto';
-
-  export default class Sha256 {
-    public update(buffer: Buffer): Hash;
-  }
-}
