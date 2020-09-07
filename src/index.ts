4845782b8f5b5cda1d2c26f1bb84085b6348b8b4
sharvariw <sharvari280596@gmail.com>
2020-09-07 20:47:35 +0530

    Tezos address encoder decoder

--- src/index.ts
+++ src/index.ts
@@ -57,17 +57,17 @@ function makeBase58CheckDecoder(p2pkhVersions: number[], p2shVersions: number[])
   return (data: string) => {
     const addr = bs58Decode(data);
     const version = addr.readUInt8(0);
-    var count = (parseInt(data.substring(2,3))-1).toString();
-    
+    const count = (parseInt(data.substring(2,3))-1).toString();
+
     if (p2pkhVersions.includes(version)) {
       return Buffer.concat([Buffer.from([0x76, 0xa9, 0x14]), addr.slice(1), Buffer.from([0x88, 0xac])]);
     } else if (p2shVersions.includes(version)) {
       return Buffer.concat([Buffer.from([0xa9, 0x14]), addr.slice(1), Buffer.from([0x87])]);
     }
-    else if (data.substring(0,2)=="tz") {
+    else if (data.substring(0,2)==="tz") {
       return Buffer.concat([Buffer.from([0x00,parseInt("0x0"+count)]), addr.slice(3)]);
     }
-    else if (data.substring(0,2)=="KT") {
+    else if (data.substring(0,2)==="KT") {
       return Buffer.concat([Buffer.from([0x01]), addr.slice(3), Buffer.from([0x00])]);
     }
     throw Error('Unrecognised address format');
