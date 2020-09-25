2727b76906c23637c4ca5b7d76feeae2210bff05
Arash Fatahzade <fatahzade@gmail.com>
2020-09-25 05:32:48 +0330

    Reformat imports

--- src/zcash.ts
+++ src/zcash.ts
@@ -4,13 +4,13 @@ Section 5.6: https://zips.z.cash/protocol/protocol.pdf
 ZIP-0173: https://zips.z.cash/zip-0173
 */
 
-import { bs58Decode, bs58Encode } from 'crypto-addr-codec';
 import {
   decode as bech32Decode,
   encode as bech32Encode,
   fromWords as bech32FromWords,
   toWords as bech32ToWords,
 } from 'bech32';
+import { bs58Decode, bs58Encode } from 'crypto-addr-codec';
 
 const BASE58_VERSION_PREFIX: { [index: string]: Buffer } = {
   t1: Buffer.from([0x1c, 0xb8]), // P2PK
