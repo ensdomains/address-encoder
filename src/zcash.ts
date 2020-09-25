0c925a39b5c7733f8a776c00e205a24c4b3abed1
Arash Fatahzade <fatahzade@gmail.com>
2020-09-25 05:22:32 +0330

    Refactore

--- src/zcash.ts
+++ src/zcash.ts
@@ -1,3 +1,9 @@
+/*
+Refernces:
+Section 5.6: https://zips.z.cash/protocol/protocol.pdf
+ZIP-0173: https://zips.z.cash/zip-0173
+*/
+
 import { bs58Decode, bs58Encode } from 'crypto-addr-codec';
 import {
   decode as bech32Decode,
@@ -6,43 +12,34 @@ import {
   toWords as bech32ToWords,
 } from 'bech32';
 
-const P2PK_VERSION = [0x1c, 0xb8];
-const P2SH_VERSION = [0x1c, 0xbd];
-const SPROUT_VERSION = [0x16, 0x9a];
-const SAPLING_PREFIX = 'zs';
+const BASE58_VERSION_PREFIX: { [index: string]: Buffer } = {
+  t1: Buffer.from([0x1c, 0xb8]), // P2PK
+  t3: Buffer.from([0x1c, 0xbd]), // P2SH
+  zc: Buffer.from([0x16, 0x9a]), // SPROUT
+};
+
+const SAPLING_PREFIX = 'zs' // SAPLING
 
 function decoder(data: string): Buffer {
-  if (data.startsWith('t1')) {
-    const addr = bs58Decode(data);
-    return Buffer.concat([Buffer.from(P2PK_VERSION), addr.slice(2)]);
-  } else if (data.startsWith('t3')) {
-    const addr = bs58Decode(data);
-    return Buffer.concat([Buffer.from(P2SH_VERSION), addr.slice(2)]);
-  } else if (data.startsWith('zc')) {
-    const addr = bs58Decode(data);
-    return Buffer.concat([Buffer.from(SPROUT_VERSION), addr.slice(2)]);
-  } else if (data.startsWith('zs')) {
+  if (data.startsWith(SAPLING_PREFIX)) {
     const { prefix, words } = bech32Decode(data);
     return Buffer.from(bech32FromWords(words));
+  } else if (Object.keys(BASE58_VERSION_PREFIX).some(prefix => data.startsWith(prefix))) {
+    const addr = bs58Decode(data);
+    return Buffer.from(addr);
   } else {
     throw Error('Unrecognised address format');
   }
 }
 
 function encoder(data: Buffer): string {
-  const version = data.slice(0, 2);
-  if (
-    Buffer.from(P2PK_VERSION).equals(version) ||
-    Buffer.from(P2SH_VERSION).equals(version) ||
-    Buffer.from(SPROUT_VERSION).equals(version)
-  ) {
+  if (Object.values(BASE58_VERSION_PREFIX).some(prefix => prefix.equals(data.slice(0, prefix.length)))) {
     return bs58Encode(data);
   } else {
     try {
       const words = bech32ToWords(data);
       return bech32Encode(SAPLING_PREFIX, words);
-    }
-    catch {
+    } catch {
       throw Error('Unrecognised address format');
     }
   }
