2ed64155c52b373e3f1e1e398d5ceeea8c1d1ecc
Arash Fatahzade <fatahzade@gmail.com>
2020-10-03 21:45:35 +0330

    Added LUNA support, closes #113

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -291,6 +291,13 @@ const vectors: Array<TestVector> = [
     ],
   },
   {
+    name: 'LUNA',
+    coinType: 330,
+    passingVectors: [
+      { text: 'terra1pdx498r0hrc2fj36sjhs8vuhrz9hd2cw0tmam9', hex: '0b4d529c6fb8f0a4ca3a84af03b397188b76ab0e' },
+    ],
+  },
+  {
     name: 'DOT',
     coinType: 354,
     passingVectors: [
--- src/index.ts
+++ src/index.ts
@@ -637,6 +637,7 @@ export const formats: IFormat[] = [
   getConfig('TRX', 195, bs58Encode, bs58Decode),
   getConfig('NEO', 239, bs58Encode, bs58Decode),
   getConfig('ALGO', 283, algoEncode, algoDecode),
+  bech32Chain('LUNA', 330, 'terra'),
   getConfig('DOT', 354, dotAddrEncoder, ksmAddrDecoder),
   getConfig('KSM', 434, ksmAddrEncoder, ksmAddrDecoder),
   getConfig('SOL', 501, bs58Encode, bs58Decode),
