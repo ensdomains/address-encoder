7308f3d6c98389f1359838cad6bbd82594865dbd
Echolon166 <Echolon166@users.noreply.github.com>
2020-10-06 15:49:54 +0300

    Add support for IOST addresses

--- src/index.ts
+++ src/index.ts
@@ -637,6 +637,7 @@ const formats: IFormat[] = [
   getConfig('TRX', 195, bs58Encode, bs58Decode),
   getConfig('NEO', 239, bs58Encode, bs58Decode),
   getConfig('ALGO', 283, algoEncode, algoDecode),
+  getConfig('IOST', 291, bs58EncodeNoCheck, bs58DecodeNoCheck),
   getConfig('DOT', 354, dotAddrEncoder, ksmAddrDecoder),
   getConfig('KSM', 434, ksmAddrEncoder, ksmAddrDecoder),
   getConfig('SOL', 501, bs58Encode, bs58Decode),
