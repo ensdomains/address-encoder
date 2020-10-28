6f58ac1d5b24b6d941588965ffebf9409ff17606
Arash Fatahzade <fatahzade@gmail.com>
2020-10-29 01:21:37 +0330

    Fixed encoding

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -171,6 +171,14 @@ const vectors: Array<TestVector> = [
     ],
   },
   {
+    name: 'ZEN',
+    coinType: 121,
+    passingVectors: [
+      { text: 'znc3p7CFNTsz1s6CceskrTxKevQLPoDK4cK', hex: '20897843a3fcc6ab7d02d40946360c070b13cf7b9795' },
+      { text: 'zswRHzwXtwKVmP8ffKKgWz6A7TB97Fuzx7w', hex: '2096b9d286b397a019f3a41ea6495dbce88d753f28a3' },
+    ],
+  },
+  {
     name: 'ZEC',
     coinType: 133,
     passingVectors: [
