d94946c9971f91bf20b2c8c70dfbad585bc34d47
Echolon166 <Echolon166@users.noreply.github.com>
2020-11-24 22:58:51 +0300

    Add new test case to HNT

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -504,6 +504,7 @@ const vectors: Array<TestVector> = [
     coinType: 904,
     passingVectors: [
       { text: '13M8dUbxymE3xtiAXszRkGMmezMhBS8Li7wEsMojLdb4Sdxc4wc', hex: '01351a71c22fefec2231936ad2826b217ece39d9f77fc6c49639926299c3869295' },
+      { text: '112qB3YaH5bZkCnKA5uRH7tBtGNv2Y5B4smv1jsmvGUzgKT71QpE', hex: '00f11444921875e2ef7435513a1d1f1b0fa49e3242956a24383912ec5d4f194077' },
     ],
   },
   {  
