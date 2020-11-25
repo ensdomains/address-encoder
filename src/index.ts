592bfda32f3e3dbaf74bf2aeaa614a6a581cd98d
Rhonin <fatahzade@gmail.com>
2020-11-25 12:29:40 +0330

    Fixing ALGO problem on browsers (#248)
    * Fixing ALGO probem on browser
    
    * Droped crypto from build

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,6 @@ import bigInt from 'big-integer';
 import { blake2b } from 'blakejs'
 import { decode as bs58DecodeNoCheck, encode as bs58EncodeNoCheck } from 'bs58';
 // @ts-ignore
-import { createHash } from 'crypto';
 import {
   b32decode,
   b32encode,
@@ -28,6 +27,7 @@ import {
   stripHexPrefix as rskStripHexPrefix,
   toChecksumAddress as rskToChecksumAddress,
 } from 'crypto-addr-codec';
+import { sha512_256 } from 'js-sha512';
 import { decode as nanoBase32Decode, encode as nanoBase32Encode } from 'nano-base32';
 import { filAddrDecoder, filAddrEncoder } from './filecoin/index';
 
@@ -584,9 +584,9 @@ const AlgoAddressByteLength = 36;
 
 // Returns 4 last byte (8 chars) of sha512_256(publicKey)
 function algoChecksum(pk: Buffer): string {
-  return createHash('SHA512-256')
+  return sha512_256
     .update(pk)
-    .digest('hex')
+    .hex()
     .substr(-AlgoChecksumByteLength * 2);
 }
 
@@ -772,7 +772,7 @@ export const formats: IFormat[] = [
   getConfig('TRX', 195, bs58Encode, bs58Decode),
   getConfig('BSV', 236, bsvAddresEncoder, bsvAddressDecoder),
   getConfig('NEO', 239, bs58Encode, bs58Decode),
-  // getConfig('ALGO', 283, algoEncode, algoDecode),
+  getConfig('ALGO', 283, algoEncode, algoDecode),
   getConfig('IOST', 291, bs58EncodeNoCheck, bs58DecodeNoCheck),
   bitcoinBase58Chain('DIVI', 301, [[0x1e]], [[0xd]]),
   bech32Chain('IOTX', 304, 'io'),
