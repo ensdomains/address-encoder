68da177ee7097a9a9c33380c023522f0fe00942c
Echolon166 <Echolon166@users.noreply.github.com>
2020-12-07 20:03:45 +0300

    Add support for STRAT addresses

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -147,6 +147,14 @@ const vectors: Array<TestVector> = [
     ]
   },
   {
+    name: 'STRAT',
+    coinType: 105,
+    passingVectors: [
+      { text: 'SdMCMmLjD6NK8ssWt5nH2gtv6XkQXErBRs', hex: '76a914b01cb711ec63be7441c350907682a73d00bf7d2888ac' },
+      { text: 'STrATiSwHPf36VbqWMUaduaN57A791YP9c', hex: '76a91447e5efb0d23a8ffa492d33df862a93e039ab622088ac' },
+    ],
+  },
+  {
     name: 'ARK',
     coinType: 111,
     passingVectors: [
