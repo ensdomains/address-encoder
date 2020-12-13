2d1f7cde840b7d942000b5bf1780f5022e7f0196
Echolon166 <erdemdeger11@hotmail.com>
2020-12-13 06:35:02 +0300

    Add support for ABBC addresses

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -496,6 +496,14 @@ const vectors: Array<TestVector> = [
     ],
   },
   {
+    name: 'ABBC',
+    coinType: 367, 
+    passingVectors: [
+      { text: 'ABBC5i3zbGsuyexJc6NaHv81yPh2WeaqrtYMMVaEqcYLz9guAAV74A', hex: '026bff3fc4dc3cde1dcb2068bef16624a260c6f0e330addb54f894bce7fa353de6' },
+      { text: 'ABBC5MTKdW6dFqEjYqQYMmLohCsALWcBAx2xRapzDTKAtz3XwKJcaf', hex: '023d3a2e33a90f8f5bcbda1ec129ba1eee5e5f2ab6a77d652cbb0517f2b49669e8' },
+    ],
+  },
+  {
     name: 'ETN',
     coinType: 415,
     passingVectors: [
