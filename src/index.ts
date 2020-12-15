5570ac9cd5560e7ae0db4f7a1e687acf57150927
ijanus <emekamicro@gmail.com>
2020-12-15 01:00:13 +0100

    Fix lint error

--- src/index.ts
+++ src/index.ts
@@ -29,8 +29,8 @@ import {
 } from 'crypto-addr-codec';
 import { sha512_256 } from 'js-sha512';
 import { decode as nanoBase32Decode, encode as nanoBase32Encode } from 'nano-base32';
-import { Keccak } from 'sha3';
 import  ripemd160  from 'ripemd160';
+import { Keccak } from 'sha3';
 import { filAddrDecoder, filAddrEncoder } from './filecoin/index';
 import { xmrAddressDecoder, xmrAddressEncoder } from './monero/xmr-base58';
 
