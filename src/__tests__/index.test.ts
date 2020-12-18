08f13dd1bdacb29d5055707fed2b28c5ce9fab88
Rhonin <fatahzade@gmail.com>
2020-12-18 16:47:49 +0330

    Added GRIN support (#263)
    * Added GRIN support
    
    * Coding style

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -728,6 +728,16 @@ const vectors: Array<TestVector> = [
     ],
   },
   {
+    name: 'GRIN',
+    coinType: 592,
+    passingVectors: [
+      { 
+        text: 'grin1k6m6sjpwc047zdhsdj9r77v5nnxm33hx7wxqvw5dhd9vl0d7t4fsaqt0lg',
+        hex: 'b6b7a8482ec3ebe136f06c8a3f79949ccdb8c6e6f38c063a8dbb4acfbdbe5d53' 
+      }
+    ],
+  },
+  {
     name: 'XDAI',
     coinType: 700,
     passingVectors: [
