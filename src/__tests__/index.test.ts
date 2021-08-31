d6091c50c7c749b395f17d416609c971656c67b7
Vincent <71333634+vinhbhn@users.noreply.github.com>
2021-08-31 18:01:27 +0700

    Add support for RUNE (ThorChain) (#298)

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -852,6 +852,14 @@ const vectors: Array<TestVector> = [
     ],
   },
   {
+    name: 'RUNE',
+    coinType: 931,
+    passingVectors: [
+      { text: 'thor1kljxxccrheghavaw97u78le6yy3sdj7h696nl4', hex: 'b7e4636303be517eb3ae2fb9e3ff3a212306cbd7' },
+      { text: 'thor1yv0mrrygnjs03zsrwrgqz4sa36evfw2a049l5p', hex: '231fb18c889ca0f88a0370d001561d8eb2c4b95d' },
+    ],
+  },
+  {
     name: 'BCD',
     coinType: 999,
     passingVectors: [
