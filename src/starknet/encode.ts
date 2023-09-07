546815501a9fc59abf68860ea35470c6c17aeb6e
Sergey Ukustov <sergey@ukstv.me>
2023-09-07 11:33:38 +0300

    use sha3

--- src/starknet/encode.ts
+++ src/starknet/encode.ts
@@ -1,9 +1,5 @@
 /* eslint-disable no-param-reassign */
 
-export function buf2hex(buffer: Uint8Array) {
-  return [...buffer].map((x) => x.toString(16).padStart(2, '0')).join('');
-}
-
 /**
  * Some function imported from https://github.com/pedrouid/enc-utils/blob/master/src/index.ts
  * enc-utils is no dependency to avoid using `Buffer` which just works in node and no browsers
