f6abcfc520020e3f18e5e854da3d3fe5d396ad80
tate <tate@ens.domains>
2023-09-20 17:45:51 +1000

    hbar

---
+++ src/coin/hbar.test.ts
@@ -0,0 +1,21 @@
+import { hexToBytes } from "@noble/hashes/utils";
+import { describe, expect, test } from "bun:test";
+import { decodeHbarAddress, encodeHbarAddress } from "./hbar";
+
+describe.each([
+  {
+    text: "255.255.1024",
+    hex: "000000ff00000000000000ff0000000000000400",
+  },
+  {
+    text: `${2n ** 32n - 1n}.${2n ** 64n - 1n}.${2n ** 64n - 1n}`,
+    hex: "ffffffffffffffffffffffffffffffffffffffff",
+  },
+])("hbar address", ({ text, hex }) => {
+  test(`encode: ${text}`, () => {
+    expect(encodeHbarAddress(hexToBytes(hex))).toEqual(text);
+  });
+  test(`decode: ${text}`, () => {
+    expect(decodeHbarAddress(text)).toEqual(hexToBytes(hex));
+  });
+});
