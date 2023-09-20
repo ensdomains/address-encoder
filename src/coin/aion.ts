4c84c1222c668bb601f34e4149475b52ae1f2614
tate <tate@ens.domains>
2023-09-20 11:45:48 +1000

    aion

---
+++ src/coin/aion.ts
@@ -0,0 +1,25 @@
+import { Coin } from "../types";
+import { bytesToHex, hexWithoutPrefixToBytes } from "../utils/bytes";
+
+const name = "AION";
+const coinType = 425;
+
+const hexRegex = /^[0-9A-Fa-f]{64}$/g;
+
+export const encodeAionAddress = (source: Uint8Array): string => {
+  if (source.length !== 32) throw new Error("Unrecognised address format");
+  return bytesToHex(source);
+};
+export const decodeAionAddress = (source: string): Uint8Array => {
+  const noPrefix = source.startsWith("0x") ? source.slice(2) : source;
+  if (noPrefix.length !== 64) throw new Error("Unrecognised address format");
+  if (!hexRegex.test(noPrefix)) throw new Error("Unrecognised address format");
+  return hexWithoutPrefixToBytes(noPrefix);
+};
+
+export const aion = {
+  name,
+  coinType,
+  encode: encodeAionAddress,
+  decode: decodeAionAddress,
+} as const satisfies Coin;
