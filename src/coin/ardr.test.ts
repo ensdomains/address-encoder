3688027c46f76467b6ad0d3c8beff59c4169df0f
tate <tate@ens.domains>
2023-09-21 14:36:03 +1000

    ardr

---
+++ src/coin/ardr.test.ts
@@ -0,0 +1,17 @@
+import { hexToBytes } from "@noble/hashes/utils";
+import { describe, expect, test } from "bun:test";
+import { decodeArdrAddress, encodeArdrAddress } from "./ardr";
+
+describe.each([
+  {
+    text: "ARDOR-MT4P-AHG4-A4NA-CCMM2",
+    hex: "15021913020e0f080a1313000a08021408",
+  },
+])("ardr address", ({ text, hex }) => {
+  test(`encode: ${text}`, () => {
+    expect(encodeArdrAddress(hexToBytes(hex))).toEqual(text);
+  });
+  test(`decode: ${text}`, () => {
+    expect(decodeArdrAddress(text)).toEqual(hexToBytes(hex));
+  });
+});
