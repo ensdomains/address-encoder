8ce1ef60b770900bda4591a7db445b310b6abf2d
tate <tate@ens.domains>
2023-09-21 14:38:02 +1000

    zel

---
+++ src/coin/zel.ts
@@ -0,0 +1,27 @@
+import { Coin } from "../types";
+import { createZcashDecoder, createZcashEncoder } from "../utils/zcash";
+
+const name = "ZEL";
+const coinType = 19167;
+
+const hrp = "za";
+const p2pkhVersions = [new Uint8Array([0x1c, 0xb8])];
+const p2shVersions = [new Uint8Array([0x1c, 0xbd])];
+
+export const encodeZelAddress = createZcashEncoder({
+  hrp,
+  p2pkhVersions,
+  p2shVersions,
+});
+export const decodeZelAddress = createZcashDecoder({
+  hrp,
+  p2pkhVersions,
+  p2shVersions,
+});
+
+export const zel = {
+  name,
+  coinType,
+  encode: encodeZelAddress,
+  decode: decodeZelAddress,
+} as const satisfies Coin;
