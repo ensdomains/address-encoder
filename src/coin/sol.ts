68792be9a1be90eb8ec5407a0cd9fb9c7a92f847
tate <tate@ens.domains>
2024-01-22 10:24:39 +1100

    SOL length validation

--- src/coin/sol.ts
+++ src/coin/sol.ts
@@ -7,8 +7,17 @@ import {
 const name = "sol";
 const coinType = 501;
 
-export const encodeSolAddress = base58UncheckedEncode;
-export const decodeSolAddress = base58UncheckedDecode;
+export const encodeSolAddress = (source: Uint8Array) => {
+  if (source.length !== 32) throw new Error("Unrecognised address format");
+
+  return base58UncheckedEncode(source);
+};
+export const decodeSolAddress = (source: string) => {
+  const decoded = base58UncheckedDecode(source);
+  if (decoded.length !== 32) throw new Error("Unrecognised address format");
+
+  return decoded;
+};
 
 export const sol = {
   name,
